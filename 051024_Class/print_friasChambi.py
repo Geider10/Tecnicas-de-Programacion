169fa7271e820fad368d5fcd6e85950d6077da67
Geider Frias <geiderde20@gmail.com>
2024-05-13 15:08:33 -0300

    i doinh the exercise 1 and 2

---
+++ 051024_Class/print_friasChambi.py
@@ -0,0 +1,27 @@
+#1. Inventario de Productos:
+#Crea un programa que almacene en una tupla los nombres de 5 productos y en un diccionario los precios de cada uno.
+#Imprime el nombre y precio de cada producto.
+ 
+products = ("manzana", "naranja", "frutillaa", "limon", "morron")
+price = {"manzana": 1000, "naranja": 1200, "frutilla":1500, "limon":1800, "morron":2000}
+
+# for ele in products:
+#     if ele in price:
+#         print("Producto: ",ele + " precio: ",price[ele]);
+
+# 2. Agenda Telefónica:
+# Crea un programa que almacene en una tupla los nombres de 10 contactos y en un diccionario los números de teléfono correspondientes. 
+# Permite al usuario buscar un contacto por nombre y mostrar su número de teléfono.
+
+names = "Pedro", "Juan", "Franco"
+number_phone = {"Pedro": 11111, "Juan":22222, "Franco":33333}
+name = input("Ingresa tu nombre para buscar tu telefono: ");
+
+# if name in names:
+#     print("Nombre: ", name, " el telefono es: ", number_phone[name]);
+# else:
+#     print("Nombre: ", name, " no esta registrado");
+     
+
+# 3. Calculadora de IMC:
+
