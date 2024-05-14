361fcdcfb4c13a39cbcf0c1ddc7aab91677b5c91
Geider Frias <geiderde20@gmail.com>
2024-05-14 15:09:29 -0300

    i doing exercise 4,5 and 6

--- 051024_Class/print_friasChambi.py
+++ 051024_Class/print_friasChambi.py
@@ -28,14 +28,60 @@ imc = (
     a4 / ((a9/100)**2),
     a5 / ((a10/100)**2),
     )
-print(imc);#nueva tupla con el imc calculado
+# print(imc);#nueva tupla con el imc calculado
 
 names = {"Pedro": "", "Juan": "", "Franco":"", "Luca": "", "Leo": ""};
 for key in names.keys():
     for value in imc:
         names[key] = value
-    print(key, "--->",names[key]);#le asigno un IMC distinto a cada valor del diccionario.
+    # print(key, "--->",names[key]);#le asigno un IMC distinto a cada valor del diccionario.
    
+# 4. Alumno y calificaciones
+names = ("Pedro", "Juan" , "Franco", "Luca", "Leo");
+notas = {"Matematica": 8,"Fisica": 10, "Quimica":6}
+cont = 0
+promedio = 0
+total = 0
+for name in names:
+    for nota in notas.keys():
+        cont+=1
+        total += notas[nota]
+    promedio = total / cont
+    print("Nombres: ",name, " promedio: ",promedio)
+    cont = 0
+    total= 0
+    promedio = 0
+
+# 5. Iventario de Ropa
+tallas = ("S","M","L","XL")
+quantity = {"S":5,"M":10,"L":15,"XL":20}
+# search = input("Ingrese la talla: ")
+# for talla in tallas:
+#     if search == talla:
+#         print("Talla: ",talla, " cantidad: ",quantity[talla]);
+#     else:
+#         print("No existe dicha talla");
+#         break;
+
+# 6. Menú de Restaurante:
+platos = "Ravioles", "Pastas", "Empanadas", "Asado", "Pastel de papas"
+pricing = { "Ravioles": 3000, "Pastas":4000,"Empanadas":5000,"Asado":6000, "Pastel de papas":7000}
+print("Menu:\n 1:Ravioles\n 2:Pastas\n 3:Empanadas\n 4:Asado\n 5:Pastel de papas")
+option = input("Escribí el plato para ver el precio: ");
+for pric in platos:
+    if option == pric:
+        print("Plato: ", pric, " precio es: ", pricing[pric])
+
+
+# 7. Biblioteca de Libros:
+books = "1984", "El principito", "El nombre del viento", "La sobra del viento", "Cien años de soledad"
+autores = {
+    "1984": "George Orwell",
+    "El principito": "Antoine de Saint-Exupéry",
+    "El nombre del viento": "Patrick Rothfuss" ,
+    "La sobra del viento":"Carlos Ruiz Zafón",
+    "Cien años de soledad": "Gabriel García Márquez"
+}
    
 
 
