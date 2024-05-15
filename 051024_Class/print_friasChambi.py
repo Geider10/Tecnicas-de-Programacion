4d0230b4798bd03cad6f309b7c4df615717d10d6
Geider Frias <geiderde20@gmail.com>
2024-05-14 22:18:04 -0300

    complet exercise 7, 8 and 9

--- 051024_Class/print_friasChambi.py
+++ 051024_Class/print_friasChambi.py
@@ -47,7 +47,7 @@ for name in names:
         cont+=1
         total += notas[nota]
     promedio = total / cont
-    print("Nombres: ",name, " promedio: ",promedio)
+    # print("Nombres: ",name, " promedio: ",promedio)
     cont = 0
     total= 0
     promedio = 0
@@ -66,11 +66,11 @@ quantity = {"S":5,"M":10,"L":15,"XL":20}
 # 6. Menú de Restaurante:
 platos = "Ravioles", "Pastas", "Empanadas", "Asado", "Pastel de papas"
 pricing = { "Ravioles": 3000, "Pastas":4000,"Empanadas":5000,"Asado":6000, "Pastel de papas":7000}
-print("Menu:\n 1:Ravioles\n 2:Pastas\n 3:Empanadas\n 4:Asado\n 5:Pastel de papas")
-option = input("Escribí el plato para ver el precio: ");
-for pric in platos:
-    if option == pric:
-        print("Plato: ", pric, " precio es: ", pricing[pric])
+# print("Menu:\n 1:Ravioles\n 2:Pastas\n 3:Empanadas\n 4:Asado\n 5:Pastel de papas")
+# option = input("Escribí el plato para ver el precio: ");
+# for pric in platos:
+#     if option == pric:
+#         print("Plato: ", pric, " precio es: ", pricing[pric])
 
 
 # 7. Biblioteca de Libros:
@@ -82,7 +82,38 @@ autores = {
     "La sobra del viento":"Carlos Ruiz Zafón",
     "Cien años de soledad": "Gabriel García Márquez"
 }
-   
+# print("Menu:\n 1:1984\n 2:El principito\n 3:El nombre del viento\n 4:La sobra del viento\n 5:Cien años de soledad")
+# option = input("Escribí el libro para ver a su autor: ");
+# for book in books:
+#     if option == book:
+#         print("Libro: ", book, " autor es: ", autores[book])
+
+# 8. Agenda de Citas:
+from datetime import datetime
+dates = ( "10/04/2024", "11/04/2024", "18/04/2024", "22/04/2024", "30/04/2024");
+citas = { "10/04/2024": "Pedro" , "11/04/2024" : "Juan", "18/04/2024": "Franco", "22/04/2024" : "Luca", "30/04/2024": "Leo"};
+# option = input("Ingresa la fecha (dd/mm/aaaa): ")
+# for date in dates:
+#     if option in date:
+#         print("fecha ", date, " la cita es: ", citas[date]);
+
+# 9. Tienda de Electrónica:
+
+# Crea un programa que almacene en una tupla los nombres de 5 productos electrónicos y en un diccionario las características de cada uno (marca, modelo, precio). Permite al usuario buscar un producto por nombre y mostrar sus características.
+products = ("telefono", "audis", "cargador", "heladera", "aire", "aire acondicionado")
+feuturs = {"Marca": "Samsung","Modelos": "z01", "Precio": 2100.60}
+option = input("Busca un producto: ")
+for product in products:
+    if(option == product):
+        print("El producto es: ", product) 
+        for key, value in feuturs.items():
+            print(key, " ---> ", value)
+            
+            
+            
+# 10. Gestión de Alumnos:
+
+# Crea un programa que almacene en una tupla los nombres de 5 alumnos y en un diccionario la información de cada uno (dirección, teléfono, fecha de nacimiento). Permite al usuario buscar un alumno por nombre y mostrar su información completa.
 
 
 
