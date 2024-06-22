02c0a80110945fcbe749b5f582a9c13a02839142
Geider Frias <geiderde20@gmail.com>
2024-06-22 00:11:15 -0300

    agregue cargar/guardar datos en un txt y termine las que realiza el usuario

--- 061424_Class/main.py
+++ 061424_Class/main.py
@@ -2,22 +2,38 @@
 # Listar todos los productos en el inventario. ✅
 # Buscar un producto por su nombre. ✅
 # Actualizar la cantidad de un producto en el inventario. ✅
-# Guardar el inventario en un archivo de texto al finalizar. ❌
+# Guardar el inventario en un archivo de texto al finalizar. ✅
 
 # funciones que realicen cada una de las tareas ✅
 # una funcion que llame las funciones segun la eleccion del usuario ✅
-# cargar el inventario en un archivo al iniciar ❌
+# cargar el inventario en un archivo al iniciar ✅
+# usar libreria OS ✅
 
 # EXTRAS:
 # eliminar producto por su key ✅
 # usar modulos para importar o exportar archivos❌
 # organizar las funciones en carpetas❌
 
-listProduct = {
-    "microfono" : { "precio" : 40000, "cantidad" : 4},
-    "mouse" : { "precio" : 10000, "cantidad" : 10},
-    "camara" : { "precio" : 80000, "cantidad" : 6}
-}
+import os
+import io #permite modificar la condificacion de texto de los archivos que se leean.
+import json #tiene metodos para guadar y cargar diccionarios como json
+
+archivo = 'file.txt'
+def loadProducts():
+    if os.path.exists(archivo) : 
+        with io.open(archivo, "r",encoding="utf-8") as file:
+            contenido = file.read()
+            return contenido
+    else:
+        print("No Existe dicho archivo")
+        
+def saveProducts():
+    #se sobrescribe el inventario en el mismo archivo
+    with open(archivo, "w") as file:
+        file.write(json.dumps(dataSet))
+
+dataSet = json.loads(loadProducts())
+
 def inputString(mensaje = "Ingresa: "):
     data = input(mensaje).lower()
     return data
@@ -28,82 +44,87 @@ def inputInt(mensaje = "Ingresa: "):
             data = int(input(mensaje))
             return data
         except ValueError:
-            print("Error, se espera un entero.")
+            print("Error, se espera un número [1 - 7].")
         
 def postProduct():
     keyProduct = inputString("Ingresa el nombre: ")
     precioProduct = inputInt("Ingresa el precio: ")
     cantidadProduct = inputInt("Ingresa la cantidad: ")
-    listProduct[keyProduct] = {"precio" : precioProduct, "cantidad" : cantidadProduct}
+    dataSet[keyProduct] = {"precio" : precioProduct, "cantidad" : cantidadProduct}
 
 def getProducts():
     cont = 1
-    for key,value in listProduct.items():
+    for key,value in dataSet.items():
         print(cont, key, value, "\n")
         cont = cont + 1
 
 def searchKeyProduct():
     product = inputString("Ingresa el nombre: ")
-    for key in listProduct.keys():
+    for key in dataSet.keys():
         if key == product:
             return product
+    
+    print("No existe dicho producto")
         
-    print("No se encontro dicho producto")
-
-def searchValuesProduct(name):
-    return listProduct.get(name)
+def searchValuesProduct():
+    product = searchKeyProduct()
+    if(product != None):
+        values = dataSet.get(product)
+        print(values)
 
 def patchProduct():
     product = searchKeyProduct()
     if(product != None):
-        # postProduct(), este puede crear un producto o editar sus valores
-        #pero esto solo edita sus valores
+        # solo edita los valores del producto
         precioProduct = inputInt("Ingresa el precio: ")
         cantidadProduct = inputInt("Ingresa la cantidad: ")
-        listProduct[product] = {"precio" : precioProduct, "cantidad": cantidadProduct}
-
-def loadProducts():
-    print("hola")
-def saveProducts():
-    print("hola")
+        dataSet[product] = {"precio" : precioProduct, "cantidad": cantidadProduct}
 
 def deleteProduct():
     product = searchKeyProduct()
-    del listProduct[product]
-    print(listProduct)
+    if(product != None):
+        del dataSet[product]
  
-def controllUser(option):
-    if(option == 1):
-        postProduct()
-    elif(option == 2):
-        getProducts()
-    elif(option == 3):
-        product = searchKeyProduct()
-        values = searchValuesProduct(product)
-        print(product, "--->", values)
-    elif(option == 4):
-        patchProduct()
-    elif(option == 5):
-        deleteProduct()
-    elif(option == 6):
-        print("aguante boca")
-
-
-
 def menu():
-    print("Bienvenido a OfiNet, tu tienda de computación.")
+    print("Bienvenido a OfiNet, tu tienda de computación.\n")
     print("1) --- Agregar un producto --- ")
     print("2) --- Listar todos los productos --- ")
     print("3) --- Buscar un producto por su nombre --- ")
     print("4) --- Actualizar los datos de un producto --- ")
     print("5) --- Eliminar un producto --- ")
     print("6) --- Guardar el inventario en un archivo --- ")
+    print("7) --- Salir ---\n")
 
-    option = inputInt("Elegí una opción: ")
-    controllUser(option)
+def controllerUser():
+    while (True):
+        menu()
+        option = inputInt("Elegí una opción del menu: ")
+
+        if(option == 1):
+            postProduct()
+            var = input()
+        elif(option == 2):
+            getProducts()
+            var = input()
+        elif(option == 3):
+            searchValuesProduct()
+            var = input()
+        elif(option == 4):
+            patchProduct()
+            var = input()
+        elif(option == 5):
+            deleteProduct()
+            var = input()
+        elif(option == 6):
+            saveProducts()
+            var = input()
+        elif(option == 7):
+            break
 
 #punto de inicio de la app
 def _main():
-    menu()
+    loadProducts()
+    controllerUser()
+
 _main()
 
